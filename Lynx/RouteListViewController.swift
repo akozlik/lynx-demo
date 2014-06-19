6e0ddecd4fd6c3976b456f6073fa6207a089f05f
Andrew Kozlik <andrew@codefortravel.com>
2014-06-19 01:19:55 -0400

    Completely fucked up route list

--- Lynx/RouteListViewController.swift
+++ Lynx/RouteListViewController.swift
@@ -18,11 +18,13 @@ class RouteListViewController : UITableViewController {
         self.title = "Routes"
         
         self.routes = loadRoutes();
-        println(routes)
+//        println(routes)
         
         for route in routes {
-            println(route.route_long_name)
+//            println(route.route_long_name)
         }
+        
+
         
         // We should now have
     }
@@ -53,11 +55,15 @@ class RouteListViewController : UITableViewController {
         return cell
     }
     
+    func getTripsForRoute(route:Route) -> Trip[] {
+        var trips : Trip[] = [];
+        
+        return trips
+        
+    }
     
-    
-    // Load routes from CSV file
-    func loadRoutes() -> Route[] {
-        var path = NSBundle.mainBundle().pathForResource("routes", ofType: "txt")
+    func loadCSVLines(filename:String, type:String) -> Dictionary<String, AnyObject>[] {
+        var path = NSBundle.mainBundle().pathForResource(filename, ofType:type)
         var string = NSString.stringWithContentsOfFile(path) as String
         
         var tempLines:String[] = string.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()) as String[]
@@ -72,28 +78,52 @@ class RouteListViewController : UITableViewController {
         }
         
         // Remove the headerline
+        var keys = lines[0].componentsSeparatedByString(",")
         lines.removeAtIndex(0)
         
-        var routes: Route[] = []
+        var returnElements : Dictionary<String, AnyObject>[] = []
         
         for line in lines {
             var elements = line.componentsSeparatedByString(",")
             
+            var returnDict : Dictionary<String, AnyObject> = Dictionary()
             // Trim strings
             for var i = 0; i < elements.count; i++ {
                 var trimmedElement = elements[i].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) as String
                 elements[i] = trimmedElement;
+
+                returnDict[keys[i]] = trimmedElement
             }
-            
-            var route_id = elements[0].toInt()
-            var route_short_name = elements[1]
-            var route_long_name = elements[2]
-            var route_type = elements[3].toInt()
-                        
-            var route = Route(route_id: route_id!, route_short_name: route_short_name, route_long_name: route_long_name, route_type: route_type!)
-            
-            routes.append(route)
+
+            returnElements.append(returnDict)
         }
+        
+        return returnElements
+    }
+    
+    // Load routes from CSV file
+    func loadRoutes() -> Route[] {
+        var elements:Dictionary<String, AnyObject>[] = loadCSVLines("routes", type: "txt") as Dictionary<String, AnyObject>[]
+        
+        var routes: Route[] = []
+        
+
+        for element in elements {
+            
+            var route_id = element["route_id"]?.integerValue
+            var route_short_name = element["route_short_name"]?.stringValue
+            var route_long_name = element["route_long_name"]?.stringValue
+            var route_type = element["route_type"]?.integerValue
+
+//            var route = Route(route_id: route_id, route_short_name: route_short_name, route_long_name: route_long_name, route_type: route_type)
+            
+//            routes.append(route)
+
+            println(route_long_name)
+
+//            println(element)
+        }
+
         
         return routes
     }
